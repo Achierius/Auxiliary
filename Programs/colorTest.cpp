8578ab9b7d5bc0a2f09b67cb2e8f959e0d9382ea
Shadow <mvrt@mvrt.com>
2015-02-16 19:44:17 -0800

    Added colorFilter.cpp and folder for completed programs, ./Programs

---
+++ Programs/colorTest.cpp
@@ -0,0 +1,132 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "enumCvType.h"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat colorFilter(Mat in, int hMin = 0, int hMax = 255, int sMin = 0, int sMax = 255, int vMin = 0, int vMax = 255, bool DEBUG = false, bool DEBUGPRE = false, bool bitAnd = true);
+
+Mat colorFilter(Mat in, int hMin = 0, int hMax = 255, int sMin = 0, int sMax = 255, int vMin = 0, int vMax = 255, bool DEBUG = false, bool DEBUGPRE = false, bool bitAnd = true)
+{
+        if(DEBUG) imshow("PreFiltered", in);
+        cvtColor(in, in, CV_BGR2HSV);
+        Mat * channels = new Mat [3];
+        split(in, channels);
+	int hueAltered = false;
+	int satAltered = false;
+	int valAltered = false;
+        if(hMin != 0 || hMax != 255)
+        {
+		hueAltered = true;
+                if(DEBUGPRE) imshow("Hue-Unfiltered", channels[0]);
+                inRange(channels[0], Scalar(hMin), Scalar(hMax), channels[0]);
+                if(DEBUG) imshow("Hue-Filtered", channels[0]);
+        }
+        if(sMin != 0 || sMax != 255)
+        {
+		satAltered = true;
+                if(DEBUGPRE) imshow("Saturation-Unfiltered", channels[1]);
+                inRange(channels[1], Scalar(sMin), Scalar(sMax), channels[1]);
+                if(DEBUG) imshow("Saturation-Filtered", channels[1]);
+		if(hueAltered && bitAnd)
+		{
+			bitwise_and(channels[0], channels[1], channels[0]);
+			imshow("asdf", channels[0]);
+			channels[1] = channels[0].clone();
+			imshow("kawaii", channels[1]);
+		}
+        }
+        if(vMin != 0 || vMax != 255)
+        {
+		valAltered = true;
+                if(DEBUGPRE) imshow("Value-Unfiltered", channels[2]);
+                inRange(channels[2], Scalar(vMin), Scalar(vMax), channels[2]);
+                if(DEBUG) imshow("Value-Filtered", channels[2]);
+        }
+	if(bitAnd)
+	{
+		if(hueAltered && valAltered && satAltered) //Sat, Value, Hue
+		{
+			bitwise_and(channels[0], channels[1], channels[0]);
+			bitwise_and(channels[0], channels[2], channels[0]);
+			channels[1] = channels[0].clone();
+			channels[2] = channels[0].clone();
+		}
+		else if(valAltered && hueAltered) //Value, Hue
+		{
+			bitwise_and(channels[0], channels[2], channels[0]);
+			channels[2] = channels[0].clone();
+			channels[1] = channels[0].clone();
+		}
+		else if(hueAltered && satAltered) //Sat, Hue
+		{
+			bitwise_and(channels[0], channels[1], channels[0]);
+			channels[2] = channels[0].clone();
+			channels[1] = channels[0].clone();
+		}
+		else if(satAltered && valAltered) //Sat, Val
+		{
+			bitwise_and(channels[2], channels[1], channels[1]);
+			channels[2] = channels[1].clone();
+			channels[0] = channels[1].clone();
+		}
+		else if(valAltered) //Only Value
+		{
+			channels[1] = channels[2].clone();
+			channels[0] = channels[2].clone();
+		}
+		else if(satAltered) //Only Sat
+		{
+			channels[2] = channels[1].clone();
+			channels[0] = channels[1].clone();
+		}
+		else if(hueAltered) //Only Hue
+		{
+			channels[1] = channels[0].clone();
+			channels[2] = channels[0].clone();
+		}
+	}
+	merge(channels, 3, in);
+	delete[] channels;
+	cvtColor(in, in, CV_HSV2BGR);
+	return in;
+}
+
+int main()
+{
+	VideoCapture camera(0);
+	Mat image;
+	namedWindow("Filtered Image", WINDOW_AUTOSIZE);
+	if(!camera.isOpened())
+	{
+		std::cout<<"Unable to open Camera\n";
+		return -1;
+	}
+	char kill = ' ';
+	int hMin = 0;
+	int hMax = 255;
+	int sMin = 0;
+	int sMax = 255;
+	int vMin = 0;
+	int vMax = 255;
+	int debugMode = 0; //0 is none, 1 is debug, 2 is debug and debugpre
+	createTrackbar("Hue Min", "Filtered Image", &hMin, 255);
+	createTrackbar("Hue Max", "Filtered Image", &hMax, 255);
+	createTrackbar("Sat Min", "Filtered Image", &sMin, 255);
+	createTrackbar("Sat Max", "Filtered Image", &sMax, 255);
+	createTrackbar("Val Min", "Filtered Image", &vMin, 255);
+	createTrackbar("Val Max", "Filtered Image", &vMax, 255);
+	createTrackbar("Debug Mode", "Filtered Image", &debugMode, 2);
+	while(kill != 's' && kill != 'q')
+	{
+		camera>>image;
+		image = colorFilter(image, hMin, hMax, sMin, sMax, vMin, vMax, debugMode>0, debugMode>1);
+		imshow("Filtered Image", image);
+		kill = waitKey(5);
+	}
+	return 0;
+}
--- Robotics/houghLines.cpp
+++ Robotics/houghLines.cpp
@@ -12,24 +12,39 @@ int main(int argc, char** argv)
 	VideoCapture camera(0);
 	Mat pre;
 	Mat image;
+	Mat writing;
 	namedWindow("Window", WINDOW_AUTOSIZE);
 	namedWindow("Image", WINDOW_AUTOSIZE);
+	writing = imread("FedExSide.jpeg");
+	pre = imread("FedExSideLines.jpeg");
+	int rho = 1;
+	int theta = 180;
+	int threshold = 49;
+	int lineMin = 9;
+	int maxGap = 49;
+	createTrackbar("Theta", "Window", &theta, 180);
+	createTrackbar("Rho", "Window", &rho, 100);
+	createTrackbar("Threshold", "Window", &threshold, 199);
+	createTrackbar("LineMin", "Window", &lineMin, 399);
+	createTrackbar("MaxGap", "Window", &maxGap, 399);
 	while(true)
 	{
-		camera>>pre;
+		/*camera>>pre;
+		imwrite("FedExSide.jpeg", pre);
+		waitKey(0);*/
+		writing = imread("FedExSide.jpeg");
 		Canny(pre, image, 50, 200, 3);
 		vector<Vec4i> lines;
 		//imshow("Image", image);
 		cout<<enumCvType(image)<<"\n";
-		waitKey(0);
-		HoughLinesP(image, lines, 1, CV_PI/180, 50, 50, 10 );
-		waitKey(0);
+		HoughLinesP(image, lines, rho, CV_PI/theta, threshold+1, lineMin+1, maxGap+1 );
 		for( size_t i = 0; i < lines.size(); i++ )
 		{
 			Vec4i l = lines[i];
-			line(pre, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+			line(writing, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
 		}
-		imshow("Window", image);
+		
+		imshow("Window", writing);
 		waitKey(1);
 	}
 	return 0;
